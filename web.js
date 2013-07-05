adb76cd0e29330a165486559a487b8bdafcc25ee
Ubuntu <ubuntu@ip-172-31-24-197.us-west-2.compute.internal>
2013-07-05 06:01:36 +0000

    Changed Hello World 2 to index.html: 1st attempt

--- web.js
+++ web.js
@@ -1,9 +1,16 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+// app.get('/', function(request, response) {
+fs.readFileSync('/index.html', function(err, html){
+    if(err){
+	return console.log(err);
+    }
+    
+    response.send(html.toString('utf8', 0));
+
 });
 
 var port = process.env.PORT || 5000;
